8f5b108c85a575e461a4671d24639e9c3f25ce15
oopsys6 <operasystem6@naver.com>
2021-03-25 00:12:16 +0900

    init setting

---
+++ src/main/java/com/server/vieco/ViEcoApplication.java
@@ -0,0 +1,13 @@
+package com.server.vieco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViEcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViEcoApplication.class, args);
+	}
+
+}
