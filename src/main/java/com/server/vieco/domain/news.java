76e7afef436e9183f1beb7bf2c12beeea7b514d4
oopsys6 <operasystem6@naver.com>
2021-04-04 00:22:46 +0900

    feat: add getter

--- src/main/java/com/server/vieco/domain/news.java
+++ src/main/java/com/server/vieco/domain/news.java
@@ -1,14 +1,19 @@
 package com.server.vieco.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class news {
 
     @Id @GeneratedValue
+    private String id;
+
     private String title;
 
     private String content;
