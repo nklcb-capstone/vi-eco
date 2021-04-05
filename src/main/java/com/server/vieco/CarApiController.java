3fd3be428ce275006816ee98b2654e83eb9063d5
oopsys6 <operasystem6@naver.com>
2021-04-05 21:53:14 +0900

    refactor: 요청 url 쿼리 형식 변경

--- src/main/java/com/server/vieco/CarApiController.java
+++ src/main/java/com/server/vieco/CarApiController.java
@@ -3,6 +3,7 @@ package com.server.vieco;
 import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedInputStream;
@@ -15,15 +16,15 @@ import java.net.URL;
 @RestController
 public class CarApiController {
 
-    @GetMapping("/api/car/electric/station")
-    public String callElectricCarApiWithJson() {
+    @GetMapping("/api/car/electric/station?numOfRows={numOfRows}&pageNo={pageNo}")
+    public String callElectricCarApiWithJson(@PathVariable Long numOfRows, @PathVariable Long pageNo) {
         StringBuffer result = new StringBuffer();
         String jsonPrintString = null;
         try {
             String apiUrl = "http://apis.data.go.kr/B552584/EvCharger/getChargerInfo?" +
                     "serviceKey=BHsquG%2FvorGlzWqsqkFMoOYe5hAUjnunPhlWdwb3CILZ2xuTDoNGD2R1E81gl7Lzmtq3n2cCYW11YUqQGVlUWA%3D%3D" +
-                    "&numOfRows=10" +
-                    "&pageNo=4";
+                    "&numOfRows=" + numOfRows +
+                    "&pageNo=" + pageNo;
             URL url = new URL(apiUrl);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.connect();
