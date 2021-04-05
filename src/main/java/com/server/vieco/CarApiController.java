8653d27493916b3194e5367aef876f819ed9270b
oopsys6 <operasystem6@naver.com>
2021-04-05 21:21:29 +0900

    refactor: api controller refactor

--- src/main/java/com/server/vieco/CarApiController.java
+++ src/main/java/com/server/vieco/CarApiController.java
@@ -13,37 +13,10 @@ import java.net.URL;
 
 
 @RestController
-public class RestTestController {
+public class CarApiController {
 
-    @GetMapping("/apitest")
-    public String callApiWithXml() {
-        StringBuffer result = new StringBuffer();
-        try {
-            String apiUrl = "http://apis.data.go.kr/B552584/EvCharger/getChargerInfo?" +
-                    "serviceKey=BHsquG%2FvorGlzWqsqkFMoOYe5hAUjnunPhlWdwb3CILZ2xuTDoNGD2R1E81gl7Lzmtq3n2cCYW11YUqQGVlUWA%3D%3D" +
-                    "&numOfRows=10" +
-                    "&pageNo=4";
-            URL url = new URL(apiUrl);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
-
-            String returnLine;
-            result.append("<xmp>");
-            while((returnLine = bufferedReader.readLine()) != null) {
-                result.append(returnLine + "\n");
-            }
-            urlConnection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return result + "</xmp>";
-    }
-
-    @GetMapping("/jsonapi")
-    public String callApiWithJson() {
+    @GetMapping("/api/car/electric/station")
+    public String callElectricCarApiWithJson() {
         StringBuffer result = new StringBuffer();
         String jsonPrintString = null;
         try {
