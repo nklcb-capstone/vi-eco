f247e4093a18658af3aaa5238901a1991ccb110f
oopsys6 <operasystem6@naver.com>
2021-04-05 21:38:10 +0900

    refactor: 모든 주소 허용

--- src/main/java/com/server/vieco/config/WebConfig.java
+++ src/main/java/com/server/vieco/config/WebConfig.java
@@ -10,7 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://ec2-3-36-231-201.ap-northeast-2.compute.amazonaws.com:8080/api/car/electric/station")
-                .allowedOrigins("localhost:3000");
+                .allowedOrigins("*");
     }
 }
