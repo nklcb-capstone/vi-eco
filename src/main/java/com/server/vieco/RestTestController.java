e76a320a09dcd1a814af960fb0598fb47ca14b38
oopsys6 <operasystem6@naver.com>
2021-04-05 20:07:31 +0900

    feat: xml -> json 코드 추가

--- src/main/java/com/server/vieco/RestTestController.java
+++ src/main/java/com/server/vieco/RestTestController.java
@@ -1,19 +1,22 @@
 package com.server.vieco;
 
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+
 
 @RestController
 public class RestTestController {
 
     @GetMapping("/apitest")
-    public String callApi() {
+    public String callApiWithXml() {
         StringBuffer result = new StringBuffer();
         try {
             String apiUrl = "http://apis.data.go.kr/B552584/EvCharger/getChargerInfo?" +
@@ -37,5 +40,33 @@ public class RestTestController {
         }
 
         return result + "</xmp>";
+    }
+
+    @GetMapping("/jsonapi")
+    public String callApiWithJson() {
+        StringBuffer result = new StringBuffer();
+        String jsonPrintString = null;
+        try {
+            String apiUrl = "http://apis.data.go.kr/B552584/EvCharger/getChargerInfo?" +
+                    "serviceKey=BHsquG%2FvorGlzWqsqkFMoOYe5hAUjnunPhlWdwb3CILZ2xuTDoNGD2R1E81gl7Lzmtq3n2cCYW11YUqQGVlUWA%3D%3D" +
+                    "&numOfRows=10" +
+                    "&pageNo=4";
+            URL url = new URL(apiUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, "UTF-8"));
+            String returnLine;
+            while((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine + "\n");
+            }
+
+            JSONObject jsonObject = XML.toJSONObject(result.toString());
+            jsonPrintString = jsonObject.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return jsonPrintString;
     }
 }
