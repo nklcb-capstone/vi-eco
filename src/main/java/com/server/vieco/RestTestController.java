bdf2bc17597a02317789e920c905edef66ec7983
oopsys6 <operasystem6@naver.com>
2021-04-05 18:02:48 +0900

    feat: 전기차 api 호출 테스트

---
+++ src/main/java/com/server/vieco/RestTestController.java
@@ -0,0 +1,41 @@
+package com.server.vieco;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@RestController
+public class RestTestController {
+
+    @GetMapping("/apitest")
+    public String callApi() {
+        StringBuffer result = new StringBuffer();
+        try {
+            String apiUrl = "http://apis.data.go.kr/B552584/EvCharger/getChargerInfo?" +
+                    "serviceKey=BHsquG%2FvorGlzWqsqkFMoOYe5hAUjnunPhlWdwb3CILZ2xuTDoNGD2R1E81gl7Lzmtq3n2cCYW11YUqQGVlUWA%3D%3D" +
+                    "&numOfRows=10" +
+                    "&pageNo=4";
+            URL url = new URL(apiUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+
+            String returnLine;
+            result.append("<xmp>");
+            while((returnLine = bufferedReader.readLine()) != null) {
+                result.append(returnLine + "\n");
+            }
+            urlConnection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result + "</xmp>";
+    }
+}
