7881d3e1e1d8f7225a6afe54e6e7460fe381e8e2
oopsys6 <operasystem6@naver.com>
2021-04-18 00:17:33 +0900

    test: test

--- src/main/java/com/server/vieco/CarApiController.java
+++ src/main/java/com/server/vieco/CarApiController.java
@@ -55,7 +55,7 @@ public class CarApiController {
     }
 
     @GetMapping("/api/car/information")
-    public CarInformation callCarInformation(@RequestParam String carName) {
+    public CarInformation callCarInformation() {
 //        CarInformation carInformation = carInformationService.findByName(carName);
         CarInformation carInformation = new CarInformation("test", "test", "test", "test", "test");
         return carInformation;
