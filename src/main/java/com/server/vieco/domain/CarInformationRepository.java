f1281641870df2e4fb573872ed9187d7ab8ac658
oopsys6 <operasystem6@naver.com>
2021-04-26 22:38:53 +0900

    feat: CarInformationRepository 이름으로 찾는 메소드 추가

--- src/main/java/com/server/vieco/domain/CarInformationRepository.java
+++ src/main/java/com/server/vieco/domain/CarInformationRepository.java
@@ -1,24 +1,13 @@
 package com.server.vieco.domain;
 
-import lombok.RequiredArgsConstructor;
+import com.server.vieco.web.dto.CarInformationResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
+import java.util.List;
 
 @Repository
 public interface CarInformationRepository extends JpaRepository<CarInformation, Long> {
-    /*
-    public CarInformation findByName(String carName) {
-        CarInformation carInformation = null;
-        try {
-            carInformation = em.createQuery("select ci from CarInformation ci where ci.carName = :carName", CarInformation.class)
-                    .setParameter("carName", carName)
-                    .getSingleResult();
-        } catch (NoResultException e) {
-        }
-        return carInformation;
-    }
-     */
+
+    public List<CarInformation> findByCarNameLike(String carName);
 }
